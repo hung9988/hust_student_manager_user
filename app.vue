8d85cfc4afd50f99f75b80a33f3ea5b9d4cd3b49
hung9988 <119072634+hung9988@users.noreply.github.com>
2023-11-26 07:04:35 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="">{{ test }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import db from "./drizzle/db";
+import { users } from "./drizzle/schema";
+const test = await db.select().from(users);
+</script>
---
+++ drizzle/0000_real_hercules.sql
@@ -0,0 +1,10 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE IF NOT EXISTS "users" (
+	"name" text NOT NULL,
+	"age" integer NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY("name","age")
+);
+
+*/
