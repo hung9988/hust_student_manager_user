5ee990e7978fbfa7f6495bb3790eb8eef0b3349a
hung9988 <119072634+hung9988@users.noreply.github.com>
2023-11-26 07:10:58 +0700

    Revert "first commit" yes This reverts commit 8d85cfc4afd50f99f75b80a33f3ea5b9d4cd3b49.

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-  schema: "./schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: "postgresql://postgres:0000@localhost:5432/postnuxt",
-  },
-};
--- drizzle/db.ts
+++
@@ -1,9 +0,0 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
-
-const queryClient = postgres(
-  "postgresql://postgres:0000@localhost:5432/postnuxt",
-);
-const db = drizzle(queryClient);
-
-export default db;
